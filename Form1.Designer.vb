5320411c824ec5d4894b3495088de63f5281fd88
Aman Rawat <neogamers21@gmail.com>
2023-04-24 20:34:24 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        resources.ApplyResources(Label1, "Label1")
+        Label1.LiveSetting = Automation.AutomationLiveSetting.Polite
+        Label1.Name = "Label1"
+        ' 
+        ' Button1
+        ' 
+        resources.ApplyResources(Button1, "Button1")
+        Button1.Name = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        resources.ApplyResources(Label2, "Label2")
+        Label2.LiveSetting = Automation.AutomationLiveSetting.Polite
+        Label2.Name = "Label2"
+        ' 
+        ' TextBox1
+        ' 
+        resources.ApplyResources(TextBox1, "TextBox1")
+        TextBox1.Name = "TextBox1"
+        ' 
+        ' TextBox2
+        ' 
+        resources.ApplyResources(TextBox2, "TextBox2")
+        TextBox2.Name = "TextBox2"
+        ' 
+        ' Label3
+        ' 
+        resources.ApplyResources(Label3, "Label3")
+        Label3.LiveSetting = Automation.AutomationLiveSetting.Polite
+        Label3.Name = "Label3"
+        ' 
+        ' TextBox3
+        ' 
+        resources.ApplyResources(TextBox3, "TextBox3")
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        ' 
+        ' Form1
+        ' 
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        TransparencyKey = Color.White
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+End Class
